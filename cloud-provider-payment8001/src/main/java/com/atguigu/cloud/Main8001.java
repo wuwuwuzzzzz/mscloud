7cb4bf69e6a32f3f626485f0321480e77d66b15a
wxz <759126349@qq.com>
2024-02-28 15:45:27 +0800

    支付服务入驻

--- cloud-provider-payment8001/src/main/java/com/atguigu/cloud/Main8001.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/cloud/Main8001.java
@@ -2,6 +2,7 @@ package com.atguigu.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import tk.mybatis.spring.annotation.MapperScan;
 
 /**
@@ -10,6 +11,7 @@ import tk.mybatis.spring.annotation.MapperScan;
  */
 @SpringBootApplication
 @MapperScan("com.atguigu.cloud.mapper")
+@EnableDiscoveryClient
 public class Main8001
 {
     public static void main(String[] args)
