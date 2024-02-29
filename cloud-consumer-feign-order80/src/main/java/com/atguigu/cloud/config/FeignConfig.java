1e6a4fe8536c7ddc1215e910de126618d58174ae
wxz <759126349@qq.com>
2024-02-29 11:07:46 +0800

    OpenFeign 日志打印功能

--- cloud-consumer-feign-order80/src/main/java/com/atguigu/cloud/config/FeignConfig.java
+++ cloud-consumer-feign-order80/src/main/java/com/atguigu/cloud/config/FeignConfig.java
@@ -1,5 +1,6 @@
 package com.atguigu.cloud.config;
 
+import feign.Logger;
 import feign.Retryer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,10 +15,15 @@ public class FeignConfig
     @Bean
     public Retryer myRetryer()
     {
-        //Feign默认配置是不走重试策略的
-        return Retryer.NEVER_RETRY;
+        // Feign默认配置是不走重试策略的
+//        return Retryer.NEVER_RETRY;
 
-        //最大请求次数为3(1+2)，初始间隔时间为100ms，重试间最大间隔时间为1s
-//        return new Retryer.Default(100, 1, 3);
+        // 最大请求次数为3(1+2)，初始间隔时间为100ms，重试间最大间隔时间为1s
+        return new Retryer.Default(100, 1, 3);
+    }
+
+    @Bean
+    Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
     }
 }
