44c5e3daeb1a6b29513a2c2d021dc099d57defef
wxz <759126349@qq.com>
2024-03-04 10:16:08 +0800

    Sentinel 热点规则

--- cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/cloud/controller/RateLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/cloud/controller/RateLimitController.java
@@ -5,6 +5,7 @@ import com.alibaba.csp.sentinel.slots.block.BlockException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -90,5 +91,32 @@ public class RateLimitController
     {
         log.error("程序逻辑异常了:{}", e.getMessage());
         return "程序逻辑异常了" + "\t" + e.getMessage();
+    }
+
+    /**
+     * @param p1 p1
+     * @param p2 p2
+     * @return java.lang.String
+     * @author wxz
+     * @date 10:04 2024/3/4
+     */
+    @GetMapping("/testHotKey")
+    @SentinelResource(value = "testHotKey", blockHandler = "dealHandlerTestHotKey")
+    public String testHotKey(@RequestParam(value = "p1", required = false) String p1, @RequestParam(value = "p2", required = false) String p2)
+    {
+        return "------testHotKey";
+    }
+
+    /**
+     * @param p1        p1
+     * @param p2        p1
+     * @param exception exception
+     * @return java.lang.String
+     * @author wxz
+     * @date 10:04 2024/3/4
+     */
+    public String dealHandlerTestHotKey(String p1, String p2, BlockException exception)
+    {
+        return "-----dealHandler_testHotKey";
     }
 }
